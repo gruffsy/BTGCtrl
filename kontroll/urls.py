225931a033dace7be120f7da0bcb329cd4004504
gruffsy <peamoen@gmail.com>
2019-11-21 01:09:34 +0100

    laget obj_detail

--- kontroll/urls.py
+++ kontroll/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pk>/', views.detail, name='detail'),
     path('test/', views.test, name="test"),
+    path('objects/<int:pk>/', views.obj_detail, name='obj_detail'),
 ]
--- kontroll/views.py
+++ kontroll/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Customer, Object
+from django.db.models import Q
+
 
 def index(request):
     customers = Customer.objects.all().order_by('month_id', 'kunde', 'bpoststed')
+    query = request.GET.get("q")
+    all = request.GET.get("a")
+
+    if query:
+        customers = customers.filter(
+            Q(kunde__icontains=query)).order_by('kunde', 'bpoststed')
+    if all:
+        customers = Customer.objects.all().order_by('kunde', 'bpoststed')
+
     context = {
         'customers': customers,
-        }
+        'query': query,
+        'all': all,
+    }
     return render(request, 'index.html', context)
 
 
@@ -18,6 +31,17 @@ def detail(request, pk):
     }
 
     return render(request, "detail.html", context)
+
+
+def obj_detail(request, pk):
+    obj = Object.objects.get(pk=pk)
+    customer = obj.customer
+    context = {
+        "customer": customer,
+        "obj": obj,
+    }
+
+    return render(request, "obj_detail.html", context)
 
 
 def test(request):
