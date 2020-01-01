c27ece4c9b0c6a3782bc280f90727c28222b59a6
gruffsy <peamoen@gmail.com>
2020-01-01 23:14:42 +0100

    import xport

--- kontroll/admin.py
+++ kontroll/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Month, Customer, Slokketype, Extinguishant, Object, ObjTr, Avvik
+
+
+@admin.register(Avvik)
+class AvvikAdmin(ImportExportModelAdmin):
+    pass
+
+
 # Register your models here.
 
 admin.site.register(Month),
@@ -8,4 +16,3 @@ admin.site.register(Slokketype),
 admin.site.register(Extinguishant),
 admin.site.register(Object),
 admin.site.register(ObjTr),
-admin.site.register(Avvik),
