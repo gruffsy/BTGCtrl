2ac06f860696d1e9e84f60b694c1cb0f690050d4
gruffsy <peamoen@gmail.com>
2020-01-04 15:56:33 +0100

    import xport

--- kontroll/admin.py
+++ kontroll/admin.py
@@ -10,14 +10,25 @@ class AvvikAdmin(ImportExportModelAdmin):
 class CustomerAdmin(ImportExportModelAdmin):
     pass
 
+
+class ExtinguishantAdmin(ImportExportModelAdmin):
+    pass
+
+
 class ObjectAdmin(ImportExportModelAdmin):
     pass
+
+
+class SlokketypeAdmin(ImportExportModelAdmin):
+    pass
+
+
 # Register your models here.
 
 admin.site.register(Month),
 admin.site.register(Customer, CustomerAdmin),
-admin.site.register(Slokketype),
-admin.site.register(Extinguishant),
+admin.site.register(Slokketype, SlokketypeAdmin),
+admin.site.register(Extinguishant, ExtinguishantAdmin),
 admin.site.register(Object, ObjectAdmin),
 admin.site.register(ObjTr),
 admin.site.register(Avvik, AvvikAdmin),
