a1352eccb77089d4d4c81e234045fc41f12393c2
gruffsy <peamoen@gmail.com>
2020-01-04 19:08:45 +0100

    import xport

--- kontroll/models.py
+++ kontroll/models.py
@@ -29,7 +29,7 @@ class Customer(models.Model):
     aktiv = models.BooleanField(default=True)
 
     def __str__(self):
-        return str(self.id) + ' | ' + self.kunde + ' | ' + self.badresse + ' | ' + self.bpoststed
+        return self.gml_id + ' | ' + self.kunde + ' | ' + self.badresse + ' | ' + self.bpoststed
 
 
 class Slokketype(models.Model):
