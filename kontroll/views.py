1a266126d1c55289e580537be8ab3c8a04312a0e
gruffsy <peamoen@gmail.com>
2020-01-30 19:26:23 +0100

    Angre - Ferdig Dokumentasjon - Ferdig

--- kontroll/views.py
+++ kontroll/views.py
@@ -307,7 +307,7 @@ class Pdf(View):
         services = objs.exclude(servicedato=None)
         kontr = objs.filter(servicedato=None, utbedret_avvik=None)
         kontr = kontr.exclude(Q(added=True) | Q(deleted=True))
-        kontrslokkere = kontr.exclude(object__extinguishant__slokketype=3).count()
+        kontrslokkere = kontr.exclude(object__extinguishant__slokketype=1).count()
         kontrs = kontr.count()
 
         kontrbrannposter = kontrs - kontrslokkere
